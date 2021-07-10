792b1f50525c0a9a4d45f79b56ccbc6202cfc05a
Rahel Piip <rahel@rackly.com>
2021-07-10 10:30:41 +0300

    Add color variables file counterpart for the styled components

---
+++ src/stylesheets/variables/_colors.tsx
@@ -0,0 +1,36 @@
+// I really wanted to use styled components but didn't
+// know how to directly use the _colors.scss file.
+const _colors = {
+  dark: "#333",
+
+  grey6: "#717175",
+  grey5: "#8b8b8f",
+  grey4: "#b4b4b8",
+  grey3: "#dcdce0",
+  grey2: "#eeeef2",
+  grey1: "#f7f7f9",
+
+  white: "#fff",
+
+  darkGreen: "#157123",
+  green: "#37a447",
+  lightGreen: "#40c353",
+
+  darkBlue: "#3f475e",
+  blue: "#305e95",
+  lightBlue: "#5787bf",
+
+  darkOrange: "#c14e04",
+  orange: "#e77b35",
+  lightOrange: "#eda538",
+
+  darkRed: "#af0419",
+  red: "#d33649",
+  lightRed: "#f22d45",
+
+  purple: "#835994",
+
+  brown: "#935f3a",
+};
+
+export default _colors;
